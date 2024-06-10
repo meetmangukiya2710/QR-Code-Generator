6e219e7be0cd9443ba9226819ce26ee09dd68573
meetmangukiya2710 <meetmangukiya104@gmail.com>
2024-06-10 15:27:50 +0530

    Initial Commit

---
+++ QR Code Generator/QR_Code_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Code_GeneratorApp.swift
+//  QR Code Generator
+//
+//  Created by R95 on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct QR_Code_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
